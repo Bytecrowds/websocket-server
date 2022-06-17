9fe0a6b9fddda509dbfe7ff68fb8e0b59fa929fb
TudorZgimbau <tudor.zgimbau@gmail.com>
2022-06-17 14:18:28 +0300

    fixed .env file loading

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 /**
  * @type {any}
  */
-//require('dotenv').config()
 
-const WebSocket = require('ws')
-const http = require('http')
-const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+if (process.env.PRODUCTION != "1" && process.env.PRODUCTION != "true")
+  require("dotenv").config();
 
+const WebSocket = require("ws");
+const http = require("http");
+const wss = new WebSocket.Server({ noServer: true });
+const setupWSConnection = require("./utils.js").setupWSConnection;
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('okay')
-})
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
 
-wss.on('connection', setupWSConnection)
+wss.on("connection", setupWSConnection);
 
-server.on('upgrade', (request, socket, head) => {
-    // You may check auth of request here..
-    /**
-     * @param {any} ws
-     */
-    const handleAuth = ws => {
-        wss.emit('connection', ws, request)
-    }
-    wss.handleUpgrade(request, socket, head, handleAuth)
-})
+server.on("upgrade", (request, socket, head) => {
+  // You may check auth of request here..
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = (ws) => {
+    wss.emit("connection", ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
 
-const port = process.env.PORT
+const port = process.env.PORT;
 server.listen(port, () => {
-    console.log(`running at '${process.env.HOST}' on port ${port}`)
-})
+  console.log(`running at '${process.env.HOST}' on port ${port}`);
+});
